7cc9bfc0d89207a8813270a1acb8b604238dcdb5
Hriddhi Dey <hriddhi.dey@myntra.com>
2018-01-02 13:17:23 +0530

    Attempting PWA changes.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-this.posts = {}
+AOS.init()
 
 document.addEventListener('mousemove', function(e) {
 	const x = e.pageX / (window.innerWidth * 10), y = e.pageY / (window.innerHeight * 10);
@@ -6,9 +6,14 @@ document.addEventListener('mousemove', function(e) {
 	bg.style.backgroundPosition = `${20 * x - 10}vh ${20 * y - 3}vh`;
 });
 
+navigator.serviceWorker && navigator.serviceWorker.register('/js/sw.js')
+.then(function(registration) {
+	console.log('Excellent, registered with scope: ', registration.scope);
+});
+
 $(function(){
 	$("#head-text").typed({
-		strings: [" "," ","Developer.", "Creative.", "Hungry.", "Hi, I'm Hriddhi."],
+		strings: [" "," ","Developer.", "Creative.", "Sleepy.", "Hi, I'm Hriddhi."],
 		typeSpeed: 10,
 		backDelay: 1000,
 		loop: false,
---
+++ js/sw.js
@@ -0,0 +1,32 @@
+document.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
+
+self.addEventListener('install', e => {
+	e.waitUntil(
+		// after the service worker is installed,
+		// open a new cache
+		caches.open('hriddhi-dey-cache').then(cache => {
+			// add all URLs of resources we want to cache
+			return cache.addAll([
+			'/',
+			'/index.html',
+			'/article.html',
+			'/styles/grid.css',
+			'/styles/style.css',
+			'/styles/animations.css',
+			'/styles/aos.css',
+			'/js/index.js',
+			'/js/typed.js',
+			'/js/aos.js',
+			'/assets/images/bg.jpg',
+			'/assets/images/logo.png',
+			'/assets/images/myface.jpg',
+			]);
+		})
+	);
+});
