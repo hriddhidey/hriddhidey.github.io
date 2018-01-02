12fe90d1aadc868d79bb15db7e472760eb1d1a85
Hriddhi Dey <hriddhi.dey@myntra.com>
2018-01-02 15:51:17 +0530

    --wip-- [skip ci]

--- js/sw.js
+++ js/sw.js
@@ -10,23 +10,25 @@ self.addEventListener('install', e => {
 	e.waitUntil(
 		// after the service worker is installed,
 		// open a new cache
-		caches.open('hriddhi-dey-cache').then(cache => {
+		caches.open('hriddhi-dey-cache')
+		.then(cache => {
 			// add all URLs of resources we want to cache
 			return cache.addAll([
-			'/',
-			'/index.html',
-			'/article.html',
-			'/styles/grid.css',
-			'/styles/style.css',
-			'/styles/animations.css',
-			'/styles/aos.css',
-			'/js/index.js',
-			'/js/typed.js',
-			'/js/aos.js',
+			// '/',
+			// '/index.html',
+			// '/article.html',
+			// '/styles/grid.css',
+			// '/styles/style.css',
+			// '/styles/animations.css',
+			// '/styles/aos.css',
+			// '/js/index.js',
+			// '/js/typed.js',
+			// '/js/aos.js',
 			'/assets/images/bg.jpg',
 			'/assets/images/logo.png',
 			'/assets/images/myface.jpg',
 			]);
 		})
+		.catch(e => console.log('Error occurred while opening cache: ', e))
 	);
 });
