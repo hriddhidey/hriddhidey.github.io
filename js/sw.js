85f122269785c1af20520c4b9d4877c7dc838337
Hriddhi Dey <hriddhi.dey@myntra.com>
2018-01-02 15:47:40 +0530

    --wip-- [skip ci]

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-document.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request).then(function(response) {
 			return response || fetch(event.request);
